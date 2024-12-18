e0d7106135fd4dfe550a66e5d26468161da264a0
Sylveria Ozioma Ihemedu <silvernasa@myyahoo.com>
2024-12-18 19:19:54 +0000

    Add Article model with fields for title, content, image, and timestamp

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    image = models.ImageField(upload_to='articles/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
