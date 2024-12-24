d22a8c3b6207751824a2b2e3d1e843cb2eccac07
Sylveria Ozioma Ihemedu <silvernasa@myyahoo.com>
2024-12-24 13:53:34 +0000

    Create script.js file and addfunction for load more

---
+++ static/js/script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const loadMoreButton = document.getElementById('load-more');
+
+    if (loadMoreButton) {
+        loadMoreButton.addEventListener('click', () => {
+            const nextPage = loadMoreButton.getAttribute('data-next-page');
+            fetch(`?page=${nextPage}`)
+                .then(response => response.text())
+                .then(data => {
+                    const parser = new DOMParser();
+                    const doc = parser.parseFromString(data, 'text/html');
+                    const newArticles = doc.querySelector('#articles-container').innerHTML;
+                    const newButton = doc.querySelector('#load-more');
+
+                    document.querySelector('#articles-container').innerHTML += newArticles;
+                    if (newButton) {
+                        loadMoreButton.setAttribute('data-next-page', newButton.getAttribute('data-next-page'));
+                    } else {
+                        loadMoreButton.remove();
+                    }
+                });
+        });
+    }
+});
