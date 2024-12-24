58de625b90b100468a4526db30a6cf4f9cca01b8
Sylveria Ozioma Ihemedu <silvernasa@myyahoo.com>
2024-12-24 13:42:56 +0000

    Modify views.py to paginate articles

--- articles/views.py
+++ articles/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from .models import Article
 
 # Create your views here.
 
 def home_view(request):
-    articles = Article.objects.all()
-    return render(request, 'articles/home.html', {'articles': articles})  
+    articles = Article.objects.all().order_by('-published_date')
+    paginator = Paginator(articles, 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number) 
+    return render(request, 'articles/home.html', {'page_obj': page_obj})  
