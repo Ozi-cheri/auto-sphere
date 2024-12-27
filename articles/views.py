a58346e5e48f0b490c7f9afaa992eddef38fa622
Sylveria Ozioma Ihemedu <silvernasa@myyahoo.com>
2024-12-27 19:36:07 +0000

    Add article view in views.py

--- articles/views.py
+++ articles/views.py
@@ -15,4 +15,8 @@ def signup_view(request):
     return render(request, 'signup.html')
 
 def login_view(request):
-    return render(request, 'login.html')   
+    return render(request, 'login.html')  
+
+def articles_view(request):  
+    articles = Article.objects.all()
+    return render(request, 'articles/articles.html', {'articles': articles})    
