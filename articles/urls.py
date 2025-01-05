736d41a718112cf63c04f14856486a1c9330085a
Sylveria Ozioma Ihemedu <silvernasa@myyahoo.com>
2025-01-05 15:26:02 +0000

    Create article_detail.html file and add content, modify urls.py

--- articles/urls.py
+++ articles/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('articles/', views.articles_view, name='articles'),
     path('articles/<int:article_id>/upvote/', views.upvote_article, name='upvote_article'),  
     path('articles/<int:article_id>/downvote/', views.downvote_article, name='downvote_article'),  
+    path('articles/<int:pk>/', views.article_detail_view, name='article_detail'), 
+    
+    
 ]
