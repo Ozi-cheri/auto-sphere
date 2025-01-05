ef8cc44ef0436012362aef32ca82f8d03d479d00
Sylveria Ozioma Ihemedu <silvernasa@myyahoo.com>
2025-01-05 14:13:41 +0000

    Add comment section to models.py and apply migrations

--- articles/models.py
+++ articles/models.py
@@ -13,3 +13,13 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, related_name='comments', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)  # Name of the commenter
+    text = models.TextField()  # Comment text
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.name} on {self.article.title}"
+       
