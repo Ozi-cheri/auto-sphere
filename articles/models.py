225a3e789335211f63a6c72d2c3424e1a3941c70
Sylveria Ozioma Ihemedu <silvernasa@myyahoo.com>
2025-01-14 12:54:17 +0000

    Add cloudinaryfield to models.py and apply migrations

--- articles/models.py
+++ articles/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+
 
 # Create your models here.
 
 class Article(models.Model):   
     title = models.CharField(max_length=200)
     content = models.TextField()
-    image = models.ImageField(upload_to='articles/', blank=True, null=True)
+    image = CloudinaryField('image', blank=True, null=True)  
     created_at = models.DateTimeField(auto_now_add=True)
     upvotes = models.PositiveIntegerField(default=0)  
     downvotes = models.PositiveIntegerField(default=0)  
